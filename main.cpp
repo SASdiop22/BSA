dc50a95a436359f2c87c1a4564c36b0fe5ab728c
Abdou Samatte Diop <151473963+SASdiop22@users.noreply.github.com>
2024-01-03 10:30:12 +0100

    Apres correction des bornes

--- main.cpp
+++ main.cpp
@@ -2,18 +2,12 @@
 #include <string>
 #include <vector>
 #include "BSA.h"
+#include "fonction.h"
 #include "benchmarks.h"
 int main()
 {
-    OriginalBSA A{1000,100,8};
-    std::vector<double> upper{5,5};
-    std::vector<double> lower{-5,-5};
-
-    //A.solve(lower,upper,rosenbrock,1.0);
-    std::vector<double> param{0.458767,0.201833,0,2.04112e-232,5.25455e-315,5.95606e-304,0.00993209,1.53124e-314};
-    std::cout << rosenbrock(param)<<std::endl;
-    std::cout << "Weierstrass(param,0.5,3.0) = "<<weierstrass(param,0.5,3.0)<<std::endl;;
-    std::cout << "Michalewics(param,10) = "<<michalewicz(param,10)<<std::endl;;
-
-
+    OriginalBSA A{10000,30,30};
+    std::vector<double> upper(30,5);
+    std::vector<double> lower(30,-5);
+    affiche(A.solve(lower,upper,rastrigin,1.0));
 }
